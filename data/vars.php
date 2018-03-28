e05c8e0124231db477ec7c7cc8dcff96ffffd415
Samuel Garcia <samg15wr@gmail.com>
2018-03-28 17:35:35 +0200

    Fichier de variables globales

---
+++ data/vars.php
@@ -0,0 +1,10 @@
+<?php
+
+define('WS_SERVER',"192.168.43.79");
+define('DB_SERVER',"127.0.0.1");
+define('DB_USER',"root");
+define('PW_USER', 'samsam31');
+define('DB_NAME','skills_detector');
+
+
+?>

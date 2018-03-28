8840ccbcda6e1bab8f52836cd28bd4c9e96e7ccf
Agardavoir <gardavoiralexis@gmail.com>
2018-03-28 09:08:26 +0200

    Ajout de la page rendez-vous
    + ajout des liens entre la page competences et la page rdv
    + modification du css

--- rendezvous.php
+++ rendezvous.php
@@ -18,7 +18,7 @@
     <body>
       <div id="top-bar">
         <div id="top-bar_wl">
-          <img style="height: 70%; width: auto; margin: 5%;" src="img/logo.png"/>
+          <a href="competences.php" ><img style="height: 70%; width: auto; margin: 5%;" src="img/logo.png"/></a>
         </div>
         <div id="top-bar_wc">
           <h1>lebonskill.fr</h1>
@@ -30,21 +30,27 @@
       </div>
       <div id="left-menu">
         <h2 class="menu_off">Mon profil</h2>
-        <a href="rendezvous.php"><h2 class="menu_off">RDV à venir</h2></a>
+        <h2 class="menu_on">RDV à venir</h2>
         <h2 class="menu_off">Messagerie</h2>
         <h2 class="menu_off">Déconnexion</h2>
       </div>
       <div id="main-content">
         <?php
-          $reponse = $bdd->query('SELECT * FROM posseder p, utilisateur u, competence c where p.id_u = u.id_u and p.id_c = c.id_c');
+          /* display des rendez-vous par tuiles contenant le titre du rdv, le lieu, la date et la personne qui est présente */
+          /* avec fake utilisateur courant */
+          $id_current = 1;
+
+          //$reponse = $bdd->query('SELECT * FROM rdv r, utilisateur u, prendre p where p.id_u = u.id_u and p.id_r = r.id_r and p.id_u = ' . $id_current);
+          $reponse = $bdd->query('SELECT r.titre, r.horaire, u.nom, u.prenom, u.photo FROM rdv r, prendre p, utilisateur u where r.id_r = p.id_r and p.id_u = u.id_u and p.id_u != '. $id_current .' and p.id_r IN (SELECT p.id_r FROM prendre p WHERE p.id_u = '. $id_current .')');
 
           while($donnees = $reponse->fetch()){
             /* la classe brick désigne les éléments prenant toutes la largueur de l'élément parent. */
-            echo '<div class="brick">';
-            echo '<img src="' . $donnees['photo'] . '"/>'; //affiche l'image
+            echo '<div class="tile">';
             echo '<h1>' . $donnees['titre'] . '</h1>'; //le titre de la competence
-            echo '<h2>' . $donnees['prenom'] . ' ' . $donnees['nom'] . '</h2>'; //le prenom et le nom de l'utilisateur
-            echo '<p>' . $donnees['desc'] . '</p>'; //la description de la competence
+            echo '<h2> Lieu : </h2>'; //le lieu est manquant dans la db ?
+            echo '<h2> Date : ' . $donnees['horaire'] . '</h2>';
+            echo '<h2> Avec ' . $donnees['prenom'] . ' ' . $donnees['nom'] . '</h2>'; //le prenom et le nom de l'utilisateur
+            echo '<img src="' . $donnees['photo'] . '"/>'; //affiche l'image
             echo '</div>';
           }
         ?>

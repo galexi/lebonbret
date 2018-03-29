d3933dbdaa9e304e84e0b0ba3e3a9444ffda4a36
Samuel Garcia <samg15wr@gmail.com>
2018-03-29 22:06:29 +0200

    Chat et prise de rdv OK

--- priserendezvous.php
+++ priserendezvous.php
@@ -3,12 +3,18 @@ include 'data/vars.php';
 session_start();
 
 //valeur fictives
-$id_u1=1;
-$id_u2=2;
-$horaire="2018-05-04 16:15:00";
-$titre="Faire des gaufres";
-$id_l="1";
+$id_u1=$_GET['id_u1'];
+$id_u2=$_GET['id_u2'];
+$horaire=$_GET['date_heure'];
+$titre=$_GET['titre'];
+$id_l=$_GET['id_l'];
+$etat=0;
 
+echo $id_u1 ."<br>";
+echo $id_u2 ."<br>";
+echo $_GET['date_heure'] ."<br>";
+echo $titre ."<br>";
+echo $id_l ."<br>";
 
 //Connexion à la base MySQL
 if($bdd = mysqli_connect(DB_SERVER, DB_USER, PW_USER, DB_NAME))
@@ -20,27 +26,23 @@ if($bdd = mysqli_connect(DB_SERVER, DB_USER, PW_USER, DB_NAME))
   }
 
 //creation du rdv
-$req_prep = mysqli_prepare($bdd, "INSERT INTO rdv (horaire, titre, etat, id_l) VALUES ('". $horaire ."', '". $titre ."', '0', '". $id_l ."')");
-//mysqli_stmt_bind_param($req_prep, "ss", $identifiant, $password);
-mysqli_stmt_execute($req_prep);
-//mysqli_stmt_bind_result($req_prep, $data['id_r']);
+$req_prep = mysqli_prepare($bdd, "INSERT INTO rdv (horaire, titre,etat, id_l) VALUES (?,?,?,?)");
+mysqli_stmt_bind_param($req_prep, "ssii", $horaire, $titre,$etat,$id_l);
 
 if(mysqli_stmt_execute($req_prep) == FALSE){
   echo "Error execute: " . mysqli_error($bdd);
 }
-
+echo "<br>OK1<br>";
 $last_id = mysqli_insert_id($bdd);
-
-
-//creation premiere prise
-$req_prep = mysqli_prepare($bdd, "INSERT INTO prendre (id_u, id_r) VALUES ('". $id_u1 ."', '". $last_id ."')");
-//mysqli_stmt_bind_param($req_prep, "ss", $identifiant, $password);
-mysqli_stmt_execute($req_prep);
+echo $last_id;
 
 //creation premiere prise
-$req_prep = mysqli_prepare($bdd, "INSERT INTO prendre (id_u, id_r) VALUES ('". $id_u2."', '". $last_id ."')");
-//mysqli_stmt_bind_param($req_prep, "ss", $identifiant, $password);
-mysqli_stmt_execute($req_prep);
+$req_prep = mysqli_prepare($bdd, "INSERT INTO prendre (id_u, id_r) VALUES (?,?),(?,?)");
+mysqli_stmt_bind_param($req_prep, "iiii", $id_u1,$last_id, $id_u2,$last_id);
+if(mysqli_stmt_execute($req_prep) == FALSE){
+  echo "Error execute: " . mysqli_error($bdd);
+}
 
-header('location: chat.php?id='. id_u .'&o='. id_u2);
+header('location: chat.php?id='. $id_u1 .'&o='. $id_u2);
+echo "<br>END";
 ?>
